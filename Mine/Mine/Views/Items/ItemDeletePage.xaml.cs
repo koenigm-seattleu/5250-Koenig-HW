cc16b6488d04e33c95aecfa4a45d1f770e27da1d
Mike Koenig <koenigm@seattleu.edu>
2021-01-18 11:36:51 -0800

    Added ItemDeletePage to Project

---
+++ Mine/Mine/Views/Items/ItemDeletePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Mine.Models;
+using Mine.ViewModels;
+
+namespace Mine.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemDeletePage: ContentPage
+    {
+        ItemReadViewModel viewModel;
+
+        public ItemDeletePage(ItemReadViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDeletePage()
+        {
+            InitializeComponent();
+
+            var item = new ItemModel
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemReadViewModel(item);
+            BindingContext = viewModel;
+        }
+
+        /// <summary>
+        /// Open the Delete page for this item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Delete_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
+
+
+/*
+await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(ViewModel)));
+await Navigation.PopAsync();
+*/
