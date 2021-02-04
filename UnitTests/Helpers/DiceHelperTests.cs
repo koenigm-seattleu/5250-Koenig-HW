f4b31dff91a4ed11f54294aa38e92019a57c99d7
Mike Koenig <koenigm@seattleu.edu>
2021-02-03 22:53:17 -0800

    Added UT RollDice Roll 1 Dice 6 to DiceHelperTests.cs

--- UnitTests/Helpers/DiceHelperTests.cs
+++ UnitTests/Helpers/DiceHelperTests.cs
@@ -20,5 +20,37 @@ namespace UnitTests.Models
             // Assert 
             Assert.AreEqual(0, result);
         }
+
+        [Test]
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1,6);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(true, result >= 1);
+            Assert.AreEqual(true, result <= 6);
+        }
+
+        [Test]
+        public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
+        {
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 1;
+
+            // Act
+            var result = DiceHelper.RollDice(1, 1);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert 
+            Assert.AreEqual(1, result);
+        }
     }
 }
